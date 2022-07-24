f699a9c4e5d602a3b692305f1ba596866c10f32d
Reinier Vargas <reinier-32@hotmail.com>
2022-07-23 22:15:46 -0500

    borrando el contenido basico de dom para comenzar con uno mas avanzado

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-const h1 = document.querySelector("h1");
-const input1 = document.querySelector("#calculo1");
-const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("#btnCalcular");
-const result = document.querySelector('#result');
-
-btn.addEventListener('click', btnOnClick);
-
-function btnOnClick(event) {
-    console.log(event)
-    event.preventDefault();
-    let number = parseInt(input1.value)+parseInt(input2.value);
-    result.innerHTML = "El resultado de la suma es: " + number; 
-}
+console.log("hola, mundito");
